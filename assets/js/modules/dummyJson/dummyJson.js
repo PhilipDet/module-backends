2f002844d0623ce61c21a873d1a48aff07d86e2e
ClasikDK <philipdethlefsen251@gmail.com>
2024-10-01 13:47:18 +0200

    DummyJson

--- assets/js/modules/dummyJson/dummyJson.js
+++ assets/js/modules/dummyJson/dummyJson.js
@@ -2,9 +2,43 @@ export default function dummyJson() {
     fetch("https://dummyjson.com/products")
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
+            console.log(data.products);
+            createProducts(data.products);
         })
         .catch((error) => {
             console.error("Error fetching the data:", error);
         });
+}
+
+function createProducts(products) {
+    const productsElement = document.getElementById("products");
+    let html = "";
+    products.forEach((product) => {
+        html += `
+            <img src="${product.thumbnail}" alt="${product.title}" data-title="${product.title}" data-description="${product.description}" />
+        `;
+    });
+    productsElement.innerHTML = html;
+
+    const images = productsElement.querySelectorAll("img");
+    images.forEach((img) => {
+        img.addEventListener("click", (event) => {
+            const thumbnail = event.target.src;
+            const title = event.target.getAttribute("data-title");
+            const description = event.target.getAttribute("data-description");
+            selectProduct(thumbnail, title, description);
+        });
+    });
+}
+
+function selectProduct(thumbnail, title, description) {
+    const selectedProductImg = document.querySelector("#selected-product img");
+    const selectedProductName = document.querySelector("#selected-product h2");
+    const selectedProductDescription = document.querySelector(
+        "#selected-product p"
+    );
+
+    selectedProductImg.src = thumbnail;
+    selectedProductName.textContent = title;
+    selectedProductDescription.textContent = description;
 }
--- assets/js/site.js
+++ assets/js/site.js
@@ -1,6 +1,4 @@
 // write cool JS hwere!!
 import dummyJson from "./modules/dummyJson/dummyJson.js";
-import theMealDb from "./modules/theMealDb/theMealDb.js";
 
 dummyJson();
-theMealDb();
